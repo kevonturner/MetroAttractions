481d81074e0a8c68bc7fabff47b90601a219e9ad
Kevin Turner <turner.merkster@gmail.com>
2015-05-16 15:00:46 -0500

    Added various input/output controllers

---
+++ MetroAttractions/InputController.m
@@ -0,0 +1,31 @@
+//
+//  InputController.m
+//  MetroAttractions
+//
+//  Created by Kevin Turner on 5/16/15.
+//  Copyright (c) 2015 Houston Hackathon. All rights reserved.
+//
+
+#import "InputController.h"
+
+@implementation InputController
+
+- (id)init
+{
+    self = [super init];
+    
+    
+    return self;
+}
+
+
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
