5644db0418fcdc69eb3ebe36d20bf0f7c50d10f8
Kevin Turner <turner.merkster@gmail.com>
2015-05-16 17:14:30 -0500

    Added Search View Controller

---
+++ MetroAttractions/SearchViewController.m
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.m
+//  MetroAttractions
+//
+//  Created by Kevin Turner on 5/16/15.
+//  Copyright (c) 2015 Houston Hackathon. All rights reserved.
+//
+
+#import "SearchViewController.h"
+
+@implementation SearchViewController
+
+@end
